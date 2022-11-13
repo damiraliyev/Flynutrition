f5191ef691fd088eadb991c5c519bbfb4bb185a0
damiraliyev <damir.aliyev.02@mail.ru>
2022-11-13 11:08:48 +0600

    remove storyboard

---
+++ Flynutrition/Flynutrition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flynutrition
+//
+//  Created by Damir Aliyev on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
