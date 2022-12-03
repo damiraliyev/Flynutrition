18467c216384e014f2c0ad5c433104d0f3f82a08
damiraliyev <damir.aliyev.02@mail.ru>
2022-12-03 23:18:15 +0600

    recalculate all components if amount is changed

--- Flynutrition/Flynutrition/ProductInfroViewController.swift
+++ Flynutrition/Flynutrition/ProductInfroViewController.swift
@@ -19,7 +19,7 @@ class ProductInfoViewController: UIViewController {
     var productName = String()
     
     let nutrientsStack = makeStackView(axis: .horizontal)
-    let proteins = NutrientInfo(name: "Proteins", amount: 20, imageName: "Proteins.pdf")
+    var proteins = NutrientInfo(name: "Proteins", amount: 20, imageName: "Proteins.pdf")
     let fats = NutrientInfo(name: "Fats", amount: 18, imageName: "Fats.pdf")
     let carbs = NutrientInfo(name: "Carbs", amount: 50, imageName: "Carbs.pdf")
     
@@ -33,11 +33,19 @@ class ProductInfoViewController: UIViewController {
     
     let addButton = makeButton(color: .systemBlue)
     
-    
+    var amount: Int = 0
+   
+    var caloriesAmount: Int = 0
     var proteinsAmount: Float = 0
     var fatsAmount: Float = 0
     var carbsAmount: Float = 0
     
+    var initialCalories: Int = 0
+    var initialProteins: Float = 0
+    var initialFats: Float = 0
+    var initialCarbs: Float = 0
+    
+    let errorLabel = UILabel()
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.prefersLargeTitles = false
@@ -85,10 +93,17 @@ class ProductInfoViewController: UIViewController {
         
         amountTextField.translatesAutoresizingMaskIntoConstraints = false
         amountTextField.backgroundColor = .clear
+        amountTextField.delegate = self
+        amountTextField.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
         
         
         lineView.translatesAutoresizingMaskIntoConstraints = false
         lineView.backgroundColor = .systemGray
+        
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.textColor = .systemRed
+        errorLabel.isHidden = true
+        errorLabel.font = UIFont.systemFont(ofSize: 15)
         
         addButton.setTitle("Add", for: .normal)
         addButton.titleLabel?.textColor = .white
@@ -111,6 +126,7 @@ class ProductInfoViewController: UIViewController {
         view.addSubview(massLabel)
         view.addSubview(amountTextField)
         view.addSubview(lineView)
+        view.addSubview(errorLabel)
         view.addSubview(addButton)
         
         NSLayoutConstraint.activate([
@@ -137,9 +153,14 @@ class ProductInfoViewController: UIViewController {
             lineView.heightAnchor.constraint(equalToConstant: 1)
         ])
         
+        NSLayoutConstraint.activate([
+            errorLabel.topAnchor.constraint(equalTo: lineView.bottomAnchor, constant: 12),
+            errorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+        
         
         NSLayoutConstraint.activate([
-            addButton.topAnchor.constraint(equalTo: lineView.bottomAnchor, constant: 24),
+            addButton.topAnchor.constraint(equalTo: errorLabel.bottomAnchor, constant: 24),
             addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
@@ -188,9 +209,15 @@ class ProductInfoViewController: UIViewController {
         carbs.amountLabel.text = String(product.carbs) + "g"
         measure = product.measure
         
+        caloriesAmount = product.calories
         proteinsAmount = product.proteins
         fatsAmount = product.fats
         carbsAmount = product.carbs
+        
+        initialCalories = Int(calorieAmountLabel.text!.dropLast(2))!
+        initialProteins = proteinsAmount
+        initialFats = fatsAmount
+        initialCarbs = carbsAmount
     }
     
     
@@ -201,14 +228,59 @@ class ProductInfoViewController: UIViewController {
             sender.alpha = 1
         }
         
+        if amountTextField.text == "0" {
+            return
+        }
+        
 
-        let productCalorieWithoutMeasure = String(calorieAmountLabel.text?.replacingOccurrences(of: " ", with: "").dropLast(2)  ?? "")
-        print("WITHOUT MEASURE +\(productCalorieWithoutMeasure)+")
-        print(Int("130"))
+        let productCalorieWithoutMeasure = String(calorieAmountLabel.text?.replacingOccurrences(of: " ", with: "").dropLast(2)  ?? "").replacingOccurrences(of: "\u{fffc}", with: "")
+       
         let productInfo = ["name": productName ,"amount": Int(amountTextField.text!) ?? 100, "calories": Int(productCalorieWithoutMeasure) ?? 0, "proteins": proteinsAmount, "fats": fatsAmount, "carbs": carbsAmount, "measure": measure
                                
         ] as [String : Any]
         
         NotificationCenter.default.post(name: Notification.Name("AddProduct"), object: nil, userInfo: productInfo)
     }
+}
+
+
+extension ProductInfoViewController: UITextFieldDelegate {
+   
+    @objc func textFieldEditingChanged(_ sender: UITextField) {
+        //Change the amount if value was changed
+        //Decided to put amountTextField.text value instead of amount, because intially there was the first one
+        // And initally amountTextField.text will be displayed and I decided leave it it like this
+        amount = Int(sender.text ?? "") ?? 0
+        amountTextField.text = String(amount)
+
+        
+        caloriesAmount = (amount * initialCalories) / 100
+        proteinsAmount = round((Float(amount) * initialProteins) / 100 * 10) / 10
+        fatsAmount = round((Float(amount) * initialFats) / 100 * 10) / 10
+        carbsAmount = round((Float(amount) * initialCarbs) / 100 * 10) / 10
+        
+        calorieAmountLabel.text = String(caloriesAmount) + "kC"
+        setCalorieLabel()
+        
+        proteins.amountLabel.text = String(proteinsAmount) + "g"
+        fats.amountLabel.text = String(fatsAmount) + "g"
+        carbs.amountLabel.text = String(carbsAmount) + "g"
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: textField.text ?? "")) {
+
+            errorLabel.text = ""
+            errorLabel.isHidden = true
+            return true
+        } else {
+            errorLabel.text = "Text field can contain only numbers."
+            errorLabel.isHidden = false
+            
+            return false
+        }
+       
+    }
+
 }
